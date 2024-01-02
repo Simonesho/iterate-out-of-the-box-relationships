71354e2440734fed4250662eb454a8a7a2dece51
Simone <144659113+Simonesho@users.noreply.github.com>
2024-01-02 18:23:56 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,33 @@
+    const data = generateRandomData();
+    data.forEach(item => {
+    }
+function generateRandomData() {
+
+}
+    return data;
+main();
+
+
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+}
+    const data = [];
+        console.log("Random Number: " + item);
+function main() {}
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    });
+    const data = generateRandomData();
+function generateRandomData() {
+    return data;
+    data.forEach(item => {
+
+
+    const data = [];
+
+    }
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+function main() {
